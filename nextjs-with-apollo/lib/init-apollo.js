ea08f57c84ca05da8e5c5434e3375b0523ba094e
Rob Brennan <rob@therobbrennan.com>
2018-10-19 18:29:36 -0700

    Apollo will connect to http://localhost:4000/graphql

--- nextjs-with-apollo/lib/init-apollo.js
+++ nextjs-with-apollo/lib/init-apollo.js
@@ -14,7 +14,7 @@ function create (initialState) {
     connectToDevTools: process.browser,
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
     link: new HttpLink({
-      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn', // Server URL (must be absolute)
+      uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
       credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers`
     }),
     cache: new InMemoryCache().restore(initialState || {})
