afd9c8bbbf9660711ada58dfce1268279b59d8c4
Rob Brennan <rob@therobbrennan.com>
2018-10-19 18:30:13 -0700

    Implemented a very basic listing of all users

--- nextjs-with-apollo/components/UserList.js
+++ nextjs-with-apollo/components/UserList.js
@@ -1,9 +1,41 @@
 import { Fragment } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import ErrorMessage from './ErrorMessage'
+
+export const allUsersQuery = gql`
+  query {
+    allUsers {
+      name
+      githubLogin
+      avatar
+    }
+  }
+`
+
+export const allUsersQueryVars = {}
 
 export default function UserList () {
   return (
-    <Fragment>
-      [PLACEHOLDER: UserList]
-    </Fragment>
+    <Query query={allUsersQuery} variables={allUsersQueryVars}>
+      {({ loading, error, data: { allUsers} }) => {
+        if (error) return <ErrorMessage message='Error loading users.' />
+        if (loading) return <div>Loading</div>
+
+        return (
+          <section>
+            <ul>
+              {allUsers && allUsers.map((user, index) => (
+                <li key={user.githubLogin}>
+                  <div>
+                    <a href={user.avatar} target='__blank'>{user.name} - {user.githubLogin}</a>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )
+      }}
+    </Query>
   )
 }
