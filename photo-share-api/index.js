43463aa64e2efb7241e3e1b3f36fe73e918f8a9b
Rob Brennan <rob@therobbrennan.com>
2018-12-02 17:19:07 -0800

    Minor correction to capitalization error

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -69,7 +69,7 @@ async function start() {
   httpServer.timeout = 5000
 
   httpServer.listen({ port: 4000 }, () =>
-    console.log(`GraphQL Server running at http://localhost:4000${server.graphqlPath}`)
+    console.log(`GraphQL server running at http://localhost:4000${server.graphqlPath}`)
   )
 }
 
