ec97f6d2940ed16bec753a2e91f8375e4ea4e7e6
Rob Brennan <rob@therobbrennan.com>
2018-12-02 17:18:49 -0800

    Created Welcome component with placeholder content

---
+++ nextjs-with-apollo/components/Welcome.js
@@ -0,0 +1,11 @@
+export default () => (
+  <article>
+    <h1>Welcome</h1>
+    If you have followed the instructions correctly, you should be able to view the following in your local environment:
+    <ul>
+      <li><a href='http://localhost:4000/playground' target='__blank'>The GraphQL playground</a></li>
+      <li><a href='http://localhost:3000/' target='__blank'>The NextJS front-end application</a></li>
+      <li><a href='http://localhost:3001/' target='__blank'>The original React front-end</a></li>
+    </ul>
+  </article>
+)
--- nextjs-with-apollo/pages/index.js
+++ nextjs-with-apollo/pages/index.js
@@ -1,8 +1,10 @@
 import App from '../components/App'
 import Header from '../components/Header'
+import Welcome from '../components/Welcome'
 
 export default () => (
   <App>
     <Header />
+    <Welcome />
   </App>
 )
